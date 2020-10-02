55f96dfe44ae5a8c34fcf17e64b38a5bc8a551b0
rcottrell0517 <rcottrell0517@yahoo.com>
2020-10-02 06:02:49 -0500

    Updates

---
+++ precache-manifest.20449aa36cfdc4545ac5e3c76eac6331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d2982ef1e3cde4e2333b1917693098",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "22f0ed15ebed81b2d615",
+    "url": "/robofriends/static/css/main.6e60a172.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f0ed15ebed81b2d615",
+    "url": "/robofriends/static/js/main.bdfa6477.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
